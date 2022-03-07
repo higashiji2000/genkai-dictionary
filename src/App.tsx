464c8cb1c08b88d4808e234f41204778aba75ae0
Haruki Higashiji <h.higashiji@4nonome.com>
2022-03-07 12:27:50 +0900

    コミット漏れ

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,4 @@
-import { useEffect } from "react";
-import "./App.css";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "./firebase";
-
 const App = () => {
-  useEffect(() => {
-    (async () => {
-      const querySnapshot = await getDocs(collection(db, "words"));
-      console.log(querySnapshot);
-      querySnapshot.forEach((doc) => {
-        console.log(doc.data());
-      });
-    })();
-  }, []);
   return <div className="App"></div>;
 };
 
