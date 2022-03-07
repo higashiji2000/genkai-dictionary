b60473fb23aa7b04b41185997471831f6f1e7743
Haruki Higashiji <h.higashiji@4nonome.com>
2022-03-07 12:01:16 +0900

    連携確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  useEffect(() => {
+    (async () => {
+      const querySnapshot = await getDocs(collection(db, "words"));
+      console.log(querySnapshot);
+      querySnapshot.forEach((doc) => {
+        console.log(doc.data());
+      });
+    })();
+  }, []);
+  return <div className="App"></div>;
+};
 
 export default App;
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseApp = initializeApp({
+export const app = initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
