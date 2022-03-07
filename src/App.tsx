3eb616ca1582cd39331446376b3b2209ab1e890f
Haruki Higashiji <h.higashiji@4nonome.com>
2022-03-07 15:27:48 +0900

    基本的なルーティングとダミーコンポーネント作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Home } from "./component/home/Home";
+import { Search } from "./component/search/Search";
+import { View } from "./component/view/View";
+
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="search" element={<Search />} />
+        <Route path="view" element={<View />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
