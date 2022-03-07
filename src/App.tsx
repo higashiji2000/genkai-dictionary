44b7a7c72222bbad4b20c8be55a1c0cc5cb32080
higashiji <83479175+higashiji2000@users.noreply.github.com>
2022-03-07 12:26:58 +0900

    Revert "Firebase#3"

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
-import { useEffect } from "react";
-import "./App.css";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "./firebase";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  useEffect(() => {
-    (async () => {
-      const querySnapshot = await getDocs(collection(db, "words"));
-      console.log(querySnapshot);
-      querySnapshot.forEach((doc) => {
-        console.log(doc.data());
-      });
-    })();
-  }, []);
-  return <div className="App"></div>;
-};
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
