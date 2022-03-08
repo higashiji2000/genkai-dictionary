333657037ccde797f406a08f4939c32280ea49e1
Haruki Higashiji <h.higashiji@4nonome.com>
2022-03-08 16:51:08 +0900

    テーマ作成

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import CssBaseLine from "@mui/material/CssBaseline";
+import { theme } from "./theme";
+import { ThemeProvider } from "@mui/material/styles";
 
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseLine>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
     </CssBaseLine>
   </React.StrictMode>,
   document.getElementById("root")
---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#f4ef60",
+    },
+    secondary: {
+      main: "#f7003a",
+    },
+  },
+});
