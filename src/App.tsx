f6543df84a5670880bf9f57d7c24b45e2f54ab59
Haruki Higashiji <h.higashiji@4nonome.com>
2022-03-08 16:35:14 +0900

    header完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Home } from "./component/home/Home";
 import { Search } from "./component/search/Search";
+import { Header } from "./component/utilities/Header";
 import { View } from "./component/view/View";
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="search" element={<Search />} />
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import CssBaseLine from "@mui/material/CssBaseline";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <CssBaseLine>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CssBaseLine>
   </React.StrictMode>,
   document.getElementById("root")
 );
