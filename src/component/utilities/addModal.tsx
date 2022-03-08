81ab922ccd8917b03921158ce5dc223c3ceb0711
Haruki Higashiji <h.higashiji@4nonome.com>
2022-03-09 01:50:48 +0900

    formを取得してコンソールに出すまで

--- src/component/utilities/addModal.tsx
+++ src/component/utilities/addModal.tsx
@@ -1,8 +1,56 @@
-import { Modal, Box } from "@mui/material";
+/** @jsxImportSource @emotion/react */
+import { Modal, Box, Typography } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { css } from "@mui/material/styles";
 
 type Props = {
   open: boolean;
   handleClose: () => void;
+};
+
+export const AddModal = (props: Props) => {
+  const { open, handleClose } = props;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ shouldUnregister: true, reValidateMode: "onSubmit" });
+  return (
+    <Modal open={open} onClose={handleClose}>
+      <Box sx={modalStyle}>
+        <Typography align="center">単語追加</Typography>
+        <form
+          action="submit"
+          onSubmit={handleSubmit(
+            (data) => {
+              console.log(data);
+            },
+            (data) => {
+              console.log("error" + data);
+            }
+          )}
+          css={form}
+        >
+          <input
+            type="text"
+            {...register("word", {
+              required: "入力必須です",
+              pattern: {
+                value: /^[あ-ん]+$/,
+                message: "ひらがなのみ入力可能です",
+              },
+            })}
+          />
+          <button>追加！！</button>
+        </form>
+        {errors.word && (
+          <Typography sx={{ color: "warning.main" }}>
+            {errors.word.message}
+          </Typography>
+        )}
+      </Box>
+    </Modal>
+  );
 };
 
 const modalStyle = {
@@ -15,11 +63,6 @@ const modalStyle = {
   width: "fit-content",
 };
 
-export const AddModal = (props: Props) => {
-  const { open, handleClose } = props;
-  return (
-    <Modal open={open} onClose={handleClose}>
-      <Box sx={modalStyle}>hello</Box>
-    </Modal>
-  );
-};
+const form = css`
+  display: flex;
+`;
